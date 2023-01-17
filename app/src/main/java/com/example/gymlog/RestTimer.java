692dc85557539a493ea5324ef18f6aee5adf5e91
DrcWinata <federic.winata@binus.ac.id>
2023-01-17 08:40:40 +0700

    resttimer2

--- app/src/main/java/com/example/gymlog/RestTimer.java
+++ app/src/main/java/com/example/gymlog/RestTimer.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.Display;
 import android.view.View;
 import android.widget.Button;
